fc54b2c0367abeb3f57edcce66353e0d6e5e738d
Gustavo Zitei Vicente <55924995+gzitei@users.noreply.github.com>
2026-07-02 22:50:12 -0300

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.ts
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s: string, k: number): number {
+        const counter: Map<string, number> = new Map()
+        let left: number = 0
+        let right: number = 0
+        let maxSize: number = 0
+        let leastFrequentChar: string = s[0]
+        for (let i = 0; i < s.length; i++) {
+            const char = s.charAt(i)
+            const charCount = counter.get(char) ?? 0
+            counter.set(char, charCount + 1)
+            const leastFrequentCount = [...counter.values()].reduce((a, b) => Math.max(a, b))
+            right = i
+            while (right - left + 1 - leastFrequentCount > k) {
+                const leftMostChar = s[left]
+                const leftMostCharCount = counter.get(leftMostChar)
+                counter.set(leftMostChar, leftMostCharCount - 1)
+                left++
+            }
+            const size = right - left + 1
+            maxSize = size > maxSize ? size : maxSize
+        }
+        return maxSize
+    }
+
+    isValidWindow(length: number, lessFrequentCount: number, k: number): boolean {
+        return length - lessFrequentCount <= k
+    }
+}
