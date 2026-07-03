bbf4c180d23fce46234881a3ba1a929a141fddfc
Gustavo Zitei Vicente <55924995+gzitei@users.noreply.github.com>
2026-07-02 22:54:14 -0300

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s: string, k: number): number {
+        const counter: Map<string, number> = new Map()
+        let left: number = 0
+        let right: number = 0
+        let maxSize: number = 0
+        let mostFrequent = 0
+        for (let i = 0; i < s.length; i++) {
+            const char = s.charAt(i)
+            counter.set(char, (counter.get(char) ?? 0) + 1)
+            mostFrequent = counter.get(char) > mostFrequent ? counter.get(char) : mostFrequent
+            right = i
+            while (right - left + 1 - mostFrequent > k) {
+                counter.set(s[left], counter.get(s[left]) - 1)
+                left++
+            }
+            const size = right - left + 1
+            maxSize = size > maxSize ? size : maxSize
+        }
+        return maxSize
+    }
+}
